ddc4a22d1f37d8bd7afee33ab82a596392f8ab14
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-05-18 22:16:55 +0100

    Initial work on fancy css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,38 +7,37 @@
   let fetching: boolean = $state(false);
 </script>
 
-<h1>Welcome to GAMERCRED&TRADE;</h1>
-<p>theres no gamorcred yet but soon thereee willl beeeeee</p>
+<div class="hero">
+  <div class="effects"></div>
+  <div class="pixels"></div>
+  <div class="card">
+    <form
+      method="POST"
+      use:enhance={() => {
+        fetching = true;
 
-<img src="images/gamer cred.jpg" alt="" />
+        return async ({ update }) => {
+          await update();
+          fetching = false;
+        };
+      }}
+    >
+      <label for="steamId">
+        <input
+          type="text"
+          name="vanity"
+          placeholder="Steam Username, ID or URL"
+          required
+          disabled={fetching}
+        />
+      </label>
+    </form>
 
-<form
-  method="POST"
-  use:enhance={() => {
-    fetching = true;
-
-    return async ({ update }) => {
-      await update();
-      fetching = false;
-    };
-  }}
->
-  <label for="steamId">
-    Enter your Steam Username, ID or URL:
-    <br />
-    <input
-      type="text"
-      name="vanity"
-      placeholder="Steam Username, ID or URL"
-      required
-      disabled={fetching}
-    />
-  </label>
-</form>
-
-{#if fetching}
-  <p>Fetching data from steam servers...</p>
-{/if}
+    {#if fetching}
+      <p>Fetching data from steam servers...</p>
+    {/if}
+  </div>
+</div>
 
 {#if form}
   <p>{form.message}</p>
@@ -53,7 +52,68 @@
 {/if}
 
 <style>
+  .hero {
+    position: relative;
+    background: url("images/sample_background.png");
+    height: 100vh;
+    box-sizing: border-box;
+    background-color: white;
+    background-size: 100% auto;
+    display: flex;
+  }
+
+  .effects {
+    position: absolute;
+    backdrop-filter: blur(10px);
+    width: 100%;
+    height: 100%;
+    opacity: 80%;
+  }
+
+  .pixels {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: url("images/white_pixel.png");
+    opacity: 10%;
+  }
+
+  .card {
+    z-index: 999;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+
+    padding: 10px 10px;
+    margin: auto;
+    width: 30vw;
+    aspect-ratio: 1.66/1;
+
+    background-color: #0c1216;
+    border-radius: 30px;
+
+    box-shadow: 0px 0px 10px black;
+  }
+
+  @media (max-width: 800px) {
+    .card {
+      width: 90%;
+    }
+  }
+
+  form {
+    width: 80%;
+    margin: auto;
+  }
+
   input[type="text"] {
-    field-sizing: content;
+    width: 100%;
+    font-size: 32px;
+    box-sizing: border-box;
+    background-color: black;
+    color: white;
+    border: 2px solid white;
+    border-radius: 10px;
   }
 </style>
