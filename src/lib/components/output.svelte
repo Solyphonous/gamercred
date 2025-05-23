859fd68c84a500558c41d05bd9334205b08efa72
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-05-23 14:49:38 +0100

    Cleaned up css on output

--- src/lib/components/output.svelte
+++ src/lib/components/output.svelte
@@ -1,47 +1,32 @@
+<script lang="ts">
+  let { outputs } = $props();
+
+  let outputHidden: boolean = $state(true);
+
+  function toggleVisible() {
+    outputHidden = !outputHidden;
+  }
+</script>
+
 <div class="output-container">
-  <div class="output">
+  <div class="output {outputHidden ? 'hidden' : ''}">
     <ul>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
-      <li>Output sample</li>
+      {#each outputs as output, index (index)}
+        <li>{index}: {output}</li>
+      {/each}
     </ul>
   </div>
-  <div class="pull">V</div>
+  <button onclick={toggleVisible}>
+    <img
+      src="images/arrow.png"
+      alt=""
+      class="arrow {outputHidden ? 'hidden' : ''}"
+    />
+  </button>
 </div>
 
 <style>
   .output-container {
-    font-family: monospace;
-    text-align: center;
     width: 25%;
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
@@ -51,6 +36,8 @@
 
     display: flex;
     flex-direction: column;
+
+    box-shadow: 0 0 7px black;
   }
 
   @media (max-width: 1000px) {
@@ -59,22 +46,44 @@
     }
   }
 
-  .pull {
-    font-size: 30px;
-    text-align: center;
+  button {
     cursor: pointer;
+    background-color: #222229;
+    border: none;
   }
 
   .output {
+    max-height: 500px;
     padding: 10px 15px;
     text-align: left;
     position: relative;
+    overflow: auto;
+    transition:
+      max-height 0.2s ease,
+      padding 0.2s;
+  }
 
-    flex: 1; /* Allow this section to grow and take up remaining space */
-    overflow: auto; /* Use auto for scroll if needed */
+  .output.hidden {
+    max-height: 0;
+    overflow: hidden;
+    padding-top: 0;
+    padding-bottom: 0;
   }
 
   ul {
     list-style-type: none;
+  }
+
+  li {
+    font-family: monospace !important;
+  }
+
+  .arrow {
+    transform: rotate(270deg);
+    transition: transform 0.2s;
+  }
+
+  .arrow.hidden {
+    transform: rotate(90deg);
   }
 </style>
