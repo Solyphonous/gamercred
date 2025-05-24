794ae823350a49aa00369f4ea1d703789a9463ff
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-05-24 18:58:22 +0100

    Make getGamerCred fetch achievement info from database for later use

--- src/lib/getGamerCred.ts
+++ src/lib/getGamerCred.ts
@@ -3,9 +3,10 @@ import { query } from "./db";
 
 export async function getGamerCred(playerAchievements: string[]) {
   let gamerCred: number = 0;
+  const achievements = [];
   for (const achievementName of playerAchievements) {
     const queryResult: QueryResult = await query(
-      `SELECT score FROM achievements WHERE achievement_name=$1`,
+      `SELECT * FROM achievements WHERE achievement_name=$1`,
       [achievementName],
     );
 
@@ -13,8 +14,19 @@ export async function getGamerCred(playerAchievements: string[]) {
       continue;
     }
 
-    gamerCred += queryResult.rows[0].score;
+    const achievement = queryResult.rows[0];
+    gamerCred += achievement.score;
+
+    achievements.push({
+      name: achievement.display_name,
+      icon: achievement.icon,
+      unlockPercentage: achievement.unlock_percentage,
+      score: achievement.score,
+    });
   }
 
-  return gamerCred;
+  return {
+    gamerCred: gamerCred,
+    achievements: achievements,
+  };
 }
