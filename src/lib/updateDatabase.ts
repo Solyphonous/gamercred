4e3b183f36ee1e6cc47b20f0b290790d8fd0b464
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-05-24 18:39:45 +0100

    Make updateDatabase save achievement icons and names

--- src/lib/updateDatabase.ts
+++ src/lib/updateDatabase.ts
@@ -1,3 +1,4 @@
+import { STEAM_API_KEY } from "$env/static/private";
 import { query } from "./db";
 
 export async function updateDatabase(ownedGame: ownedGame) {
@@ -11,30 +12,61 @@ export async function updateDatabase(ownedGame: ownedGame) {
     [ownedGame.appid.toString(), ownedGame.name],
   );
 
-  const response = await fetch(
+  // Fetch achievement percentages for game
+  const percentagesResponse = await fetch(
     `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${ownedGame.appid}&format=json`,
   );
-
-  if (!response.ok) {
-    console.log(`Failed achievement get for ${ownedGame.name}`);
+  if (!percentagesResponse.ok) {
+    console.log(`Failed achievement percentage get for ${ownedGame.name}`);
     return;
   }
+  const percentageData = await percentagesResponse.json();
+  const achievementPercentages: AchievementPercentage[] =
+    percentageData.achievementpercentages.achievements;
 
-  const data = await response.json();
+  // Fetch achievement details for game
+  const achievementQuery = new URLSearchParams({
+    key: STEAM_API_KEY,
+    appid: ownedGame.appid.toString(),
+  });
+  const achievementsResponse = await fetch(
+    `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2?${achievementQuery.toString()}`,
+  );
+  if (!achievementsResponse.ok) {
+    console.log(`Failed achievement info get for ${ownedGame.name}`);
+  }
+  const achievementData = await achievementsResponse.json();
 
-  const achievements: achievement[] = data.achievementpercentages.achievements;
+  let achievementInfo: AchievementInfo[];
+
+  if (achievementData.game.availableGameStats.achievements) {
+    achievementInfo = achievementData.game.availableGameStats.achievements;
+  } else {
+    achievementInfo = [];
+  }
+
+  const infoMap = new Map<string, AchievementInfo>(
+    achievementInfo.map((item) => [item.name, item]),
+  );
+
+  const achievements = achievementPercentages.map((percentage) => ({
+    ...percentage,
+    ...infoMap.get(percentage.name),
+  }));
 
   for (const achievement of achievements) {
     await query(
       `
-      INSERT INTO achievements (achievement_name, appid, unlock_percentage)
-      VALUES ($1, $2, $3)
+      INSERT INTO achievements (achievement_name, appid, display_name, icon, unlock_percentage)
+      VALUES ($1, $2, $3, $4, $5)
       ON CONFLICT (achievement_name) DO
-      UPDATE SET unlock_percentage = $3
+      UPDATE SET unlock_percentage = $5
       `,
       [
         achievement.name,
         ownedGame.appid.toString(),
+        achievement.displayName.toString(),
+        achievement.icon.toString(),
         achievement.percent.toString(),
       ],
     );
