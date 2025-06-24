f413d7bea2250703060e56843f780150755a5273
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-06-24 13:39:40 +0100

    update deploy.sh to suck way less

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+set -e
+
+REMOTE_DIR="/srv/gamercred"
 
 eval $(ssh-agent -s)
+trap "ssh-agent -k" EXIT
 ssh-add ~/.ssh/digitalOcean
 docker build . -t gamercred
 docker save gamercred > gamercred.tar
-rsync -avz --progress gamercred.tar docker-compose.yml .env.production init.sql digitalOcean:/home/soly/gamercred/
+rsync -avz --progress gamercred.tar docker-compose.yml .env.production init.sql "digitalOcean:${REMOTE_DIR}/"
 
-ssh digitalOcean "cd /home/soly/gamercred/ && \
-docker load < gamercred.tar && \
-mv .env.production .env && \
-docker-compose down && \
-docker-compose up -d && \
-exit"
+ssh digitalOcean <<EOF
+
+cd "${REMOTE_DIR}"
+docker load < gamercred.tar
+mv .env.production .env
+docker-compose down
+docker-compose up -d
+exit
+
+EOF
+
 rm gamercred.tar
 
 ssh-agent -k
