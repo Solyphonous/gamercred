e33fa2ace70a6c14d2c9f372c3f962a22cbf62a1
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2026-03-05 17:17:04 +0000

    remove debug route because I fixed it (was setting secrets in wrong place in cloudflare, oops)

--- src/routes/api/debug/+server.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '$env/dynamic/private';
-export function GET() {
-  return new Response(JSON.stringify({
-    hasUrl: !!env.DATABASE_URL,
-    hasKey: !!env.DATABASE_CERTIFICATE,
-  }));
-}
