881d133eda9e38ed3a45a4e53ac30238409fd785
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2026-03-05 21:36:41 +0000

    add checks to prevent db from running if no db secrets are present

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,19 +1,23 @@
 import { Pool, type QueryResult, type QueryResultRow } from "pg";
 import { env } from "$env/dynamic/private";
 
+if (!env.DATABASE_CERTIFICATE || !env.DATABASE_URL) {
+  throw new Error("Database secrets missing!");
+}
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  ssl: {
+    ca: Buffer.from(env.DATABASE_CERTIFICATE, "base64").toString(),
+    rejectUnauthorized: true,
+  }
+});
+
 export async function query(
   text: string,
   params: string[] = [],
 ): Promise<QueryResult<QueryResultRow>> {
   try {
-    const pool = new Pool({
-      connectionString: env.DATABASE_URL,
-      ssl: {
-        ca: Buffer.from(env.DATABASE_CERTIFICATE, "base64").toString(),
-        rejectUnauthorized: true,
-      }
-    });
-
     const result: QueryResult<QueryResultRow> = await pool.query(text, params);
     return result;
   } catch (error) {
