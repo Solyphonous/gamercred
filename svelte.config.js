30bfad1ca986e84faa908848d47b1898aacaac43
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2026-03-05 21:45:49 +0000

    specify runtime in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const config = { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
+const config = {
+    preprocess: vitePreprocess(), kit: {
+        adapter: adapter({
+            runtime: "nodejs24.x"
+        })
+    }
+};
 
 export default config;
